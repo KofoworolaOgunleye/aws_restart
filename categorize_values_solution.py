e4b237a3a081ed939f8f810cec4abbd1e59ead77
KofoworolaOgunleye <kay16th@yahoo.com>
2021-01-07 12:12:09 +0000

    Update categorize_values_solution.py

--- categorize_values_solution.py
+++ categorize_values_solution.py
@@ -1,4 +1,6 @@
 # Lab 7: Categorize values into an appropriate data type
+# This program prints the datatype of each value stored in myMixedbagList using for loop to loop
+# through the list and the format method
 
 # Exercise 1: Mixed Bag
 myMixedBagList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
